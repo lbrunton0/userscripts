3bba98603a18cfa165508202c1573c9cb42489db
Lucas Brunton <lucas.brunton@fullymanaged.com>
2023-07-19 13:02:10 -0400

    Auto expanding Emails on legacy GCI Case View.

--- GCI Fixes.user.js
+++ GCI Fixes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GCI Fixes
 // @namespace    https://fullymanaged.service-now.com/
-// @version      0.2
+// @version      0.3
 // @description  Fixing some issues with how GCI shows things, and try to take over the world!
 // @author       Lucas Brunton
 // @match        https://fullymanaged.service-now.com/*
@@ -24,24 +24,36 @@ addGlobalStyle('* {max-width: 100%;}')
 addGlobalStyle('.modal-lg {margin: .5% !important; width: 99% !important;}')
 addGlobalStyle('#email_preview_iframe {margin: .5%  !important; width: 99%  !important;}')
 
-if (/\/email_display\.do/.test (location.pathname) && window.top !== window.self) {
+if (/\/x_carew_default_ca_fm_case\.do/.test(location.pathname)) {
+    let streamActions = document.querySelectorAll('a.stream-action');
+    for (let i = 0; i < streamActions.length; i++) {
+        if (streamActions[i].innerText === "Show email details") {
+            streamActions[i].click();
+        }
+        if (streamActions[i].innerText === "Hide email details") {
+            streamActions[i].hide();
+        }
+    }
+}
+
+if (/\/email_display\.do/.test(location.pathname) && window.top !== window.self) {
     let iframe = window.parent.document.querySelector('iframe[src^="' + location.pathname + location.search + '"]');
     let children = document.body.children;
     let height = 0;
     for (let i = 0; i < children.length; i++) {
         height += children[i].scrollHeight;
     }
-    height = Math.max(height, document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+    height = Math.max(height, document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
     iframe.style.height = height + 'px';
 
-    window.addEventListener('resize', function(event){
+    window.addEventListener('resize', function (event) {
         let children = document.body.children;
         let height = 0;
         for (let i = 0; i < children.length; i++) {
             height += children[i].scrollHeight;
         }
         iframe.style.height = height + 'px';
-        height = Math.max(height, document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+        height = Math.max(height, document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
         iframe.style.height = height + 'px';
     });
 }
